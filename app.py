1237b8ab178fd52c23e79c96d019f969b9e6997b
dubhunter <the@dubhunter.com>
2013-12-13 01:29:36 -0800

    add logging

--- app.py
+++ app.py
@@ -17,14 +17,17 @@ GPIO.setup(PIN_SWITCH, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 def switch():
     try:
         if GPIO.input(PIN_SWITCH):
+            print "Taking down the curtain..."
             requests.delete(ENDPOINT, timeout=TIMEOUT)
             GPIO.output(PIN_LED, True)
         else:
+            print "Putting up the curtain..."
             requests.post(ENDPOINT, timeout=TIMEOUT)
             GPIO.output(PIN_LED, False)
     except Exception:
         pass
 
 while 1:
+    print "Waiting..."
     GPIO.wait_for_edge(PIN_SWITCH, GPIO.BOTH)
     switch()
